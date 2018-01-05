63316cab84700cac610551beaa242d13fc7a0e01
Rong Jiang <maikfangogoair@gmail.com>
2018-01-05 19:30:13 +0800

    assignment3 fractal

--- assignment3_Fractals/README.md
+++ assignment3_Fractals/README.md
@@ -1,5 +1,8 @@
 Assignment details:  
 http://web.stanford.edu/class/cs106b/assn/Fractals.html
 
-
+Demo:  
+<p align="center">
+<img src="res/fractal_demo.png" width="500"/>
+<p/>
 
--- assignment3_Fractals/src/fractals.cpp
+++ assignment3_Fractals/src/fractals.cpp
@@ -1,15 +1,37 @@
-// This is the CPP file you will edit and turn in. (TODO: Remove this comment!)
+// This is the CPP file you will edit and turn in.
 
 #include "fractals.h"
+#include "math.h"
 using namespace std;
 
-void drawSierpinskiTriangle(GWindow& window, double x, double y, double size, int order) {
-    // TODO: write this function
+void drawTriagnle(GWindow& window, double x1, double y1, \
+                  double x2, double y2, double x3, double y3){
+    window.drawLine(x1, y1, x2, y2);
+    window.drawLine(x2, y2, x3, y3);
+    window.drawLine(x3, y3, x1, y1);
+}
 
+void drawMultipleST(GWindow& window, double x1, double y1, double x2, double y2, double x3, double y3, int order_left){
+    drawTriagnle(window, (x1+x2)/2, (y1+y2)/2, (x2+x3)/2, (y2+y3)/2, (x1+x3)/2, (y1+y3)/2 );
+    if (order_left > 1){
+        drawMultipleST(window,x1, y1 ,(x1+x2)/2, (y1+y2)/2, (x1+x3)/2, (y1+y3)/2 , order_left-1);
+        drawMultipleST(window,(x1+x2)/2, (y1+y2)/2, x2, y2 , (x2+x3)/2, (y2+y3)/2, order_left-1);
+        drawMultipleST(window,(x1+x3)/2, (y1+y3)/2 ,(x2+x3)/2, (y2+y3)/2, x3, y3 , order_left-1);
+    }
+}
+
+void drawSierpinskiTriangle(GWindow& window, double x, double y, double size, int order) {
+    if (order > 0 && size > 0 && x > 0 && y > 0){
+        drawTriagnle(window, x, y, x+size, y, x+size/2, y+size/2*sqrt(3));
+        if (order > 1){
+            int order_left = order - 1;
+            drawMultipleST(window, x, y, x+size, y, x+size/2, y+size/2*sqrt(3), order_left);
+        }
+    }else if (size > 0 || x > 0 || y > 0){
+        throw string("invalid inputs");
+    }
 }
 
 int floodFill(GWindow& window, int x, int y, int color) {
-    // TODO: write this function
-
     return 0;   // this is only here so it will compile
 }
